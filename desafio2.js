09e1d3fb716b36c47ac06c2e686b8fc71ae076e1
Fabio Aragao Tessaro <fabiot92@gmail.com>
2023-11-20 01:25:21 -0300

    desafio DIO
    utilisar node desafio2.js

---
+++ desafio2.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+function calcularSaldoRankeadas(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+}
+
+
+rl.question('Digite a quantidade de vitórias: ', (vitorias) => {
+    rl.question('Digite a quantidade de derrotas: ', (derrotas) => {
+       
+        const resultado = calcularSaldoRankeadas(parseInt(vitorias), parseInt(derrotas));
+        console.log(resultado);
+
+        
+        rl.close();
+    });
+});
